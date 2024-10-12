7bc1f63ac8ae621f71bc26e0d91a5795e968648b
dimig13 <145656829+GiaDimi13@users.noreply.github.com>
2024-10-12 16:52:33 +0200

    small change DTO

--- backend-java/SpringCloudPXL/department-service/src/main/java/be/pxl/services/department/domain/dto/EmployeeDTO.java
+++ backend-java/SpringCloudPXL/department-service/src/main/java/be/pxl/services/department/domain/dto/EmployeeDTO.java
@@ -1,8 +1,16 @@
 package be.pxl.services.department.domain.dto;
 
 import jakarta.persistence.Embeddable;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
 
 @Embeddable
+@Data
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
 public class EmployeeDTO {
     private Long id;
 
